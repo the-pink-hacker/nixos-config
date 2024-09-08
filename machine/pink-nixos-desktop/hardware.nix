2907fd3750f6cd71886c019102903e494dca83cf
Pink Garrett <pink@thepinkhacker.com>
2024-09-08 02:35:02 -0400

    Add exec option to drives This fixes so many things

--- machine/pink-nixos-desktop/hardware.nix
+++ machine/pink-nixos-desktop/hardware.nix
@@ -5,15 +5,16 @@ let
 	"user"
 	"users"
 	"nofail"
+        "exec"
     ];
 in {
-    fileSystems."/mnt/Documents0" = {
+    fileSystems."/run/media/pink/Documents0" = {
         device = "/dev/disk/by-uuid/78e98f36-e6fb-465c-86f7-26524ebcad5b";
         fsType = "ext4";
         options = driveOptions;
     };
 
-    fileSystems."/mnt/Documents1" = {
+    fileSystems."/run/media/pink/Documents1" = {
         device = "/dev/disk/by-uuid/661a481f-7f16-4f3a-8de6-f52d4de8c856";
         fsType = "ext4";
         options = driveOptions;
