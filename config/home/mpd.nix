056d8abc73829a33cfe12dd12379bb0b17d66876
Pink Garrett <pink@thepinkhacker.com>
2024-11-04 03:59:58 -0500

    MPD Discord RPC

--- config/home/mpd.nix
+++ config/home/mpd.nix
@@ -20,4 +20,23 @@
             { key = "K"; command = [ "select_item" "scroll_up" ]; }
         ];
     };
+
+    services.mpd-discord-rpc = {
+        enable = true;
+        settings = {
+            id = 677226551607033903;
+            hosts = [
+                "127.0.0.1:6600"
+            ];
+            format = {
+                details = "$title";
+                state = "$artist | $album [$duration]";
+                timestamp = "elapsed";
+                large_image = "notes";
+                small_image = "notes";
+                large_text = "";
+                small_text = "MPD on NixOS";
+            };
+        };
+    };
 }
