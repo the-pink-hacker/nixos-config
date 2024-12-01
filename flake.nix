14d730dd95c64fb3882eb21947839c5de3153a80
Pink Garrett <pink@thepinkhacker.com>
2024-12-01 04:45:01 -0500

    Switch nixpkgs to 24.11

--- flake.nix
+++ flake.nix
@@ -10,9 +10,9 @@
     description = "A simple NixOS flake.";
 
     inputs = {
-    	nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    	nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.11";
 	home-manager = {
-	    url = "github:nix-community/home-manager";
+	    url = "github:nix-community/home-manager/release-24.11";
 	    inputs.nixpkgs.follows = "nixpkgs";
 	};
         nur.url = "github:nix-community/nur";
