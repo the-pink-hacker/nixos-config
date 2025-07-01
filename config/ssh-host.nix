0351b388897afd0db0d0c4f8f09b43df6bee5d3b
Pink Garrett <pink@thepinkhacker.com>
2025-06-30 20:36:25 -0400

    Tweak ssh

--- config/ssh-host.nix
+++ config/ssh-host.nix
@@ -1,10 +1,32 @@
 { ... }:
 
-{
+let 
+    port = 64102;
+in {
     services.openssh = {
         enable = true;
         openFirewall = true;
+        ports = [
+            port
+        ];
+        settings = {
+            #KbdInteractiveAuthentication = false;
+            #PasswordAuthentication = false;
+            PermitRootLogin = "no";
+            AllowUsers = [
+                "pink"
+            ];
+        };
+        startAgent = true;
     };
 
     programs.gnupg.agent.enableSSHSupport = true;
+
+    services.fail2ban.enable = true;
+
+    services.endlessh = {
+        enable = true;
+        openFirewall = true;
+        port = 22;
+    };
 }
